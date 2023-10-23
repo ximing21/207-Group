50546c6e99b1129805fc4621d40492bb795a4b7f
ximing21 <ximing.shen@mail.utoronto.ca>
2023-10-23 10:09:08 -0400

    added User entity

--- src/entity/User.java
+++ src/entity/User.java
@@ -3,9 +3,49 @@ package entity;
 import java.util.ArrayList;
 
 public class User {
-    Integer id;
-    String name;
-    ArrayList<Task> tasks;
-    Pomodoro pomodoro;
-    Statistics staData;
+    private final Integer id;
+
+    private final String name;
+
+    private final ArrayList<Task> tasks;
+
+    private final Pomodoro pomodoro;
+
+    private final Statistics staData;
+
+    public User(Integer id, String name, ArrayList<Task> tasks, Pomodoro pomodoro, Statistics staData) {
+        this.id = id;
+        this.name = name;
+        this.tasks = tasks;
+        this.pomodoro = pomodoro;
+        this.staData = staData;
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+        this.tasks = null;
+        this.pomodoro = null;
+        this.staData = null;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public Pomodoro getPomodoro() {
+        return pomodoro;
+    }
+
+    public Statistics getStaData() {
+        return staData;
+    }
 }
