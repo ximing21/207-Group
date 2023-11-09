c5d3aaf6ed053b8bd83f4ba0695159e1b0846ee3
ximing21 <ximing.shen@mail.utoronto.ca>
2023-11-09 12:14:25 -0500

    Modified UI GUI design

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,15 +4,22 @@ import api.TodoistDB;
 import interface_adapter.ViewManagerModel;
 import interface_adapter.add_project.AddProjectViewModel;
 import interface_adapter.added_project.AddedProjectViewModel;
+import okhttp3.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 import view.AddProjectView;
 import view.ViewManager;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
+
         // Build the main program window, the main panel containing the
         // various cards, and the layout, and stitch them together.
 
