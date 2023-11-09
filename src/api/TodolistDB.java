ff3af3dc2ef0514651c3908b07a149d87bd4a409
ximing21 <ximing.shen@mail.utoronto.ca>
2023-11-09 12:28:28 -0500

    Implemented getTasks method

--- src/api/TodolistDB.java
+++ src/api/TodolistDB.java
@@ -1,7 +1,6 @@
 package api;
 import entity.Project;
 import entity.Task;
-import use_case.add_project.AddProjectDataAccessInterface;
 
 public interface TodolistDB {
     void createProject(String name);
