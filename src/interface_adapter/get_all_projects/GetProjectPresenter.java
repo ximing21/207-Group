9824a2593450e152534014f47f40f459799522e9
emsu3617 <emilysu.pj@gmail.com>
2023-11-26 13:15:05 -0500

    Deleted and updated GetProjectPresenter

--- src/interface_adapter/get_all_projects/GetProjectPresenter.java
+++ src/interface_adapter/get_all_projects/GetProjectPresenter.java
@@ -1,10 +1,6 @@
 package interface_adapter.get_all_projects;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.added_project.AddedProjectState;
-import interface_adapter.added_project.AddedProjectViewModel;
-import use_case.add_project.AddProjectOutputBoundary;
-import use_case.add_project.AddProjectOutputData;
 import use_case.get_all_projects.GetProjectOutputBoundary;
 import use_case.get_all_projects.GetProjectOutputData;
 
@@ -24,7 +20,7 @@ public class GetProjectPresenter implements GetProjectOutputBoundary {
         GetProjectState getProjectState = getProjectViewModel.getState();
         getProjectState.setProjects(response.getPojects());
         getProjectViewModel.setState(getProjectState);
-        getProjectViewModel.firePropertyChanged(); // 触发视图更新
+        getProjectViewModel.firePropertyChanged(); // triggers update UI
     }
 
     @Override
