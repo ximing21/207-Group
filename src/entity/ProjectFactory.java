70915c10e542c63f2ff068c9ebe7ab278117ffd9
Xuanyi Lyu <xuanyi.lyu@mail.utoronto.ca>
2023-11-26 15:49:52 -0500

    Code Smells: Some pointless, unneeded things exist.

--- src/entity/ProjectFactory.java
+++ src/entity/ProjectFactory.java
@@ -1,7 +1,5 @@
 package entity;
 
-import java.time.LocalDateTime;
-
 public class ProjectFactory {
     public Project create(String projectId, String projectName, int taskCount) {
         return new Project(projectId, projectName, taskCount);
