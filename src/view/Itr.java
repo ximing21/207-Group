fbfad117fdc47c450fa9e45c90836003bc418869
ximing21 <ximing.shen@mail.utoronto.ca>
2023-11-27 11:08:36 -0500

    Added AddProjectTestCase

---
+++ src/view/Itr.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.util.Iterator;
+
+public class Itr implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
