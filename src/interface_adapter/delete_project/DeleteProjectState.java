67224a6da96731f8018ca5524d97f7e1b3a49d36
emsu3617 <emilysu.pj@gmail.com>
2023-11-27 09:55:57 -0500

    Decreased unused import and code  in DeleteProjectState.java and GetProjectState.java

--- src/interface_adapter/delete_project/DeleteProjectState.java
+++ src/interface_adapter/delete_project/DeleteProjectState.java
@@ -2,14 +2,6 @@ package interface_adapter.delete_project;
 
 public class DeleteProjectState {
     private String project_name = "";
-    private Integer count = 0;
-    private String project_nameError = null;
-
-    public DeleteProjectState(DeleteProjectState copy) {
-        project_name = copy.project_name;
-        project_nameError = copy.project_nameError;
-        count = copy.count;
-    }
 
     public void setProject_name(String project_name) {
         this.project_name = project_name;
