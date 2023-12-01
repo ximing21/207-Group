f536edfd687159e37f85fbcce5a7e1961c56477a
ximing21 <ximing.shen@mail.utoronto.ca>
2023-11-30 23:42:53 -0500

    Deleted outdated class

--- src/entity/ProjectFactory.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public class ProjectFactory {
-    public Project create(String projectId, String projectName, int taskCount) {
-        return new Project(projectId, projectName, taskCount);
-    }
-}
